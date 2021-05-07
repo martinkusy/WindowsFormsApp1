1b06e69bb45b21a75139e5fb609b67b4f7f72906
kusym.03 <kusym.03@spst.eu>
2021-05-07 08:58:35 +0200

    DruheTlacitko

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -20,6 +20,17 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("ahoj");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
